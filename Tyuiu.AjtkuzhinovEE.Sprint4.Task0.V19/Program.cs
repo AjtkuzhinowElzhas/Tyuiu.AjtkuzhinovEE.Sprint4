886cd4dc51f97b7f8521929c15e4a45315e4531d
AjtkuzhinovElzhas <aitkuzinow8@gmail.com>
2025-11-05 22:22:29 +0300

    Task0

---
+++ Tyuiu.AjtkuzhinovEE.Sprint4.Task0.V19/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.AjtkuzhinovEE.Sprint4.Task0.V19.Lib;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService dataService = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int[] array = { 9, 5, 7, 4, 5, 3, 7, 8, 9, 1 };
+
+        int res = dataService.GetMultOddArrEl(array);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
