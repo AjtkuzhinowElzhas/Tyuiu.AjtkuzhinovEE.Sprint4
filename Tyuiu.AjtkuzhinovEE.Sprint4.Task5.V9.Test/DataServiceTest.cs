e1d5bd720e14c88aed014c1c4a0036ad9f3846ac
AjtkuzhinovElzhas <aitkuzinow8@gmail.com>
2025-11-11 21:19:19 +0300

    Task5

---
+++ Tyuiu.AjtkuzhinovEE.Sprint4.Task5.V9.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.AjtkuzhinovEE.Sprint4.Task5.V9.Lib;
+
+namespace Tyuiu.AjtkuzhinovEE.Sprint4.Task5.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int[,] matrix =
+            {
+                { -2, 3, 0, 1,-1 },
+                { 4, -3, 2, 0, 1 },
+                { 0, 0, -4, 3, 2 },
+                {-1, -2, 1, 4, 0 },
+                { 3,  2,-3, 0,-4 }
+
+            };
+            // всего 11 положительных элементов (3,1,4,2,1,3,2,1,4,3,2)
+            int wait = 11;
+            int result = ds.Calculate(matrix);
+            Assert.AreEqual(wait, result);
+
+
+
+        }
+    }
+}
---
+++ Tyuiu.AjtkuzhinovEE.Sprint4.Task5.V9/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.AjtkuzhinovEE.Sprint4.Task5.V9.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Random rnd = new Random();
+
+        Console.WriteLine("###########################################################################");
+        Console.WriteLine("# ИСХОДНЫЕ ДАННЫЕ:                                                        #");
+        Console.WriteLine("###########################################################################");
+        Console.WriteLine("Введите количество строк в массиве :                                       ");
+        int rows = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите количество столбцов в массива:                                     ");
+        int columns = Convert.ToInt32(Console.ReadLine());
+
+        int[,] matrix = new int[rows, columns];
+        Console.WriteLine("###########################################################################");
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                matrix[i, j] = rnd.Next(-4, 4);
+            }
+        }
+        Console.WriteLine("\nМассив:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.WriteLine($"{matrix[i, j]}   \t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("###########################################################################");
+        Console.WriteLine("# РЕЗУЛЬТАТ:                                                              #");
+        Console.WriteLine("###########################################################################");
+        int result = ds.Calculate(matrix);
+        Console.WriteLine("Количество положительных элементов массива = " + result);
+        Console.ReadKey();
+
+    }
+}
